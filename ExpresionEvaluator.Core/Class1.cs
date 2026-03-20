572e6f37656f560380b56b9b6f0d79bbeb360e0a
camilohl24 <juancamilohernandezhl@gmail.com>
2026-03-20 18:38:39 -0500

    Generate evaluator from infix expressions to postfix

--- ExpresionEvaluator.Core/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace ExpresionEvaluator.Core
-{
-    public class Class1
-    {
-
-    }
-}
--- ExpressionEvaluator.UI.Console/Program.cs
+++ ExpressionEvaluator.UI.Console/Program.cs
@@ -1,1 +1,4 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using ExpresionEvaluator.Core;
+
+var resultado = Evaluator.Evaluate("2 + 3 * (4 - 1)");
+Console.WriteLine($"El resultado es: {resultado}");
